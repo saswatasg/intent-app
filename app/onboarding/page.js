9fb5998dc5832bd1d1a5c032d0cdd8f75ac5f5ba
Saswata <saswata@Saswatas-MacBook-Pro.local>
2026-05-09 02:08:03 +0530

    fix: import useEffect in onboarding page

--- app/onboarding/page.js
+++ app/onboarding/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { ENTRY_MODULE } from '@/lib/modules'
 import styles from './page.module.css'
